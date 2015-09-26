f2c9b9e029ff5dd40c753a6f1b3358ad3f67511c
weolopez <weolopez@gmail.com>
2015-09-26 09:20:19 -0400

    updated search

--- build/app/pages/wall/wall.js
+++ build/app/pages/wall/wall.js
@@ -3,60 +3,69 @@
 
     angular.module('pages.wall', [
         'pages.search',
-        'pages.panels'
-        ])
-        .config(function ($stateProvider, $urlRouterProvider) {
-            /** Default route for page */
-            $urlRouterProvider.when('/wall', '/wall/splash');
-            $stateProvider
-                .state('wall', {
-                    url: '/wall',
-                    abstract: 'true',
-                    templateUrl: 'app/pages/wall/wall.html',
-                    controller: 'wallCtrl as vm'
-                })
-                .state('wall.splash', {
-                    url: '/splash',
-                    views: {
-                        'wallContent': {
-                            templateUrl: 'app/pages/wall/templates/splash.html'
-                        }
-                    }
-                })
+        'pages.panels',
+        'ngToast'
+    ])
+            .config(function ($stateProvider, $urlRouterProvider) {
+                /** Default route for page */
+                $urlRouterProvider.when('/wall', '/wall/splash');
+                $stateProvider
+                        .state('wall', {
+                            url: '/wall',
+                            abstract: 'true',
+                            templateUrl: 'app/pages/wall/wall.html',
+                            controller: 'wallCtrl as vm'
+                        })
+                        .state('wall.splash', {
+                            url: '/splash',
+                            views: {
+                                'wallContent': {
+                                    templateUrl: 'app/pages/wall/templates/splash.html'
+                                }
+                            }
+                        })
 
-                .state('wall.donate', {
-                    url: '/donate',
-                    views: {
-                        'wallContent': {
-                            templateUrl: 'app/pages/wall/templates/donate.html'
-                        }
-                    }
-                })
+                        .state('wall.donate', {
+                            url: '/donate',
+                            views: {
+                                'wallContent': {
+                                    templateUrl: 'app/pages/wall/templates/donate.html'
+                                }
+                            }
+                        })
 
-                .state('wall.visualsearch', {
-                    url: '/visualsearch',
-                    views: {
-                        'wallContent': {
-                            templateUrl: 'app/pages/wall/templates/visualsearch.html'
-                        }
-                    }
-                })
+                        .state('wall.visualsearch', {
+                            url: '/visualsearch',
+                            views: {
+                                'wallContent': {
+                                    templateUrl: 'app/pages/wall/templates/visualsearch.html'
+                                }
+                            }
+                        })
 
-                .state('wall.browse', {
-                    url: '/browse',
-                    views: {
-                        'wallContent': {
-                            templateUrl: 'app/pages/wall/templates/browse.html'
-                        }
-                    }
-                });
-        })
-        .controller('wallCtrl', wallCtrl);
+                        .state('wall.browse', {
+                            url: '/browse',
+                            views: {
+                                'wallContent': {
+                                    templateUrl: 'app/pages/wall/templates/browse.html'
+                                }
+                            }
+                        });
+            })
+            .controller('wallCtrl', wallCtrl);
 
-    wallCtrl.$inject = ['$log', '$ionicSideMenuDelegate'];
+    wallCtrl.$inject = ['$log', '$ionicSideMenuDelegate', 'ngToast'];
 
-    function wallCtrl($log, $ionicSideMenuDelegate) {
+    function wallCtrl($log, $ionicSideMenuDelegate, ngToast) {
         var vm = this;
+        vm.search = function () {
+            vm.isSearching = !vm.isSearching;
+
+            ngToast.create({
+                className: 'danger',
+                content: 'Searching for: '+vm.searchValue
+            });
+        }
     }
 })();
 
