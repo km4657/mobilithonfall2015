268d9dd86a74ba624962fb958afaa268ffb1548a
Lopez, Mauricio (ml5174) <ml5174@att.com>
2015-09-27 16:43:14 -0400

    added preload

--- src/app/components/panel/panel.js
+++ src/app/components/panel/panel.js
@@ -2,43 +2,79 @@
     'use strict';
 
     angular.module('components.panel', [])
-            .directive('panel', panel);
+        .directive('panel', panel);
     panel.$inject = ['$log', 'WallService', '$ionicGesture', '$ionicScrollDelegate', '$timeout'];
     function panel($log, WallService, $ionicGesture, $ionicScrollDelegate, $timeout) {
         return {
             restrict: 'E',
-            scope: true,
-            bindToController: {
+            scope: {
                 panelid: '=',
-                render: '='
+                renderid: '='
             },
             controller: PanelCtrl,
-            controllerAs: 'vm',
             templateUrl: 'app/components/panel/panel.html',
             link: function (scope, element, attrs) {
-                //$ionicScrollDelegate.zoomBy(.3);
+            // attribute names change to camel case
             }
         }
     }
 
-    PanelCtrl.$inject = ['WallService', '$timeout', '$ionicScrollDelegate'];
-    function PanelCtrl(WallService, $timeout, $ionicScrollDelegate) {
-        var vm = this;
-        vm.WallService = WallService;
+    PanelCtrl.$inject = ['$log', 'WallService', '$timeout', '$ionicScrollDelegate', '$scope'];
+    function PanelCtrl($log, WallService, $timeout, $ionicScrollDelegate, $scope) {
+        var vm = $scope;
+        
+        $scope.$watch('renderid', function(newValue) {
+            if (vm.panelid === undefined) return false;
+            if (vm.renderid === undefined) return false;
+            $scope.show();
+        })
+        
+        function init() {
+            if (null === vm.panelDirection) {
+                vm.panelDirection = vm.panelid.charAt(vm.panelid.length - 1);
+                vm.justification = (vm.panelDirection === "E" ? "left" : "right");
+            }
+        }
 
-        vm.show = function () {
-            if (vm.panelid === vm.render)
+        $scope.show = function () {
+            if (Math.abs(vm.renderid - vm.panelid) <= 2){
+                $log.debug('vm.panelid: ' + vm.panelid + ' vm.renderid: ' + vm.renderid);
+                if (vm.rows === undefined) {
+                    WallService.getPanel(vm.panelid).then(function (result) {
+                        vm.rows=[];
+                        angular.forEach(result.data, function (value, key) {
+                            if (vm.rows[value.rowNumber] === undefined)
+                                vm.rows[value.rowNumber] = [];
+                            vm.rows[value.rowNumber].push(value);
+                        });
+                        vm.isShowing=true;
+                        $timeout(function() {
+                            $ionicScrollDelegate.$getByHandle(vm.panelid).resize();
+                        },1000);
+                    });
+                }
                 return true;
-            if (vm.panelid === vm.render - 1)
-                return true;
-            if (vm.panelid === vm.render + 1)
-                return true;
+            }
             return false;
         }
-        
-        $timeout(function(){
-            $ionicScrollDelegate.resize();
-        }, 5000);
+
+        vm.getRow = function (row) {
+
+            var returnString = "";
+            if (vm.panelDirection === "W")
+                returnString = "*";
+
+            angular.forEach(row, function (value, key) {
+                returnString = returnString + value.wallName + " * ";
+            })
+
+            if (vm.panelDirection === "E")
+                returnString = returnString.substring(0, returnString.length - 2);
+
+            return returnString;
+        }
+
+        init();
     }
 })();
 
