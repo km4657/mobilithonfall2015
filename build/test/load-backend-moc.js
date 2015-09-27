f1c5098b77ed2fa2c7350105d525ab073dbfc6fc
weolopez <weolopez@gmail.com>
2015-09-27 12:14:24 -0400

    added mocking framework and fixed panel(s)

--- build/test/load-backend-moc.js
+++ build/test/load-backend-moc.js
@@ -6,10 +6,11 @@
         ])
         .run(LoadBackendMock);
 
-    LoadBackendMock.$inject = ['$httpBackend', '$http', 'RestConstants'];
-    function LoadBackendMock($httpBackend, $http, RestConstants) {
+    LoadBackendMock.$inject = ['$httpBackend', '$http', 'RestConstants', '$log'];
+    function LoadBackendMock($httpBackend, $http, RestConstants, $log) {
         angular.forEach(RestConstants, function (value, key) {
             $http.get('test/mocks/' + key + '.json').then(function (result) {
+                $log.debug('Mocking: '+value);
                 $httpBackend
                     .whenGET(value)
                     .respond(result.data);
